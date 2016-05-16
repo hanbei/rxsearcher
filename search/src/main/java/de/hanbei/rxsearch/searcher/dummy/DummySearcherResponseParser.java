62e5aa4eb6212fca12b902e0c8afa76f69c007b5
Florian Schulz <hanbei@hanbei.de>
2016-05-16 20:39:22 +0200

    response parser for dummy searcher

---
+++ search/src/main/java/de/hanbei/rxsearch/searcher/dummy/DummySearcherResponseParser.java
@@ -0,0 +1,55 @@
+package de.hanbei.rxsearch.searcher.dummy;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.Charsets;
+import com.ning.http.client.Response;
+import de.hanbei.rxsearch.model.SearchResult;
+import de.hanbei.rxsearch.searcher.ResponseParser;
+import rx.Observable;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+public class DummySearcherResponseParser implements ResponseParser {
+
+    private static final ObjectMapper mapper = new ObjectMapper();
+    private String name;
+
+    public DummySearcherResponseParser(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public Observable<SearchResult> toSearchResults(Response response) {
+        checkNotNull(response);
+
+        List<SearchResult> results = new ArrayList<>();
+
+        try {
+            String responseBodyAsString = response.getResponseBody(Charsets.UTF_8.name());
+            JsonNode jsonNode = mapper.readTree(responseBodyAsString);
+            for (JsonNode offer : jsonNode) {
+                String url = getFieldStringValue(offer, "url");
+                String title = getFieldStringValue(offer, "title");
+                String icon = getFieldStringValue(offer, "imageUrl");
+                results.add(new SearchResult(url, title, name, icon));
+            }
+        } catch (IOException e) {
+            return Observable.error(e);
+        }
+        return Observable.from(results);
+    }
+
+    private String getFieldStringValue(JsonNode node, String fieldName) {
+        String result = "";
+        JsonNode sub_node = node.findValue(fieldName);
+        if (sub_node != null) {
+            result = sub_node.asText();
+        }
+        return result;
+    }
+}
