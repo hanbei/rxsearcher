9c3c72ba2180b73aac1f33857312719d7c44f2bf
Florian Schulz <hanbei@hanbei.de>
2016-08-26 22:57:15 +0200

    Allow clients to subscribe on searcher success and errors

--- search/src/main/java/de/hanbei/rxsearch/coordination/SearcherErrorHandler.java
+++ search/src/main/java/de/hanbei/rxsearch/coordination/SearcherErrorHandler.java
@@ -2,17 +2,11 @@ package de.hanbei.rxsearch.coordination;
 
 import de.hanbei.rxsearch.model.Offer;
 import de.hanbei.rxsearch.searcher.SearcherException;
-import org.slf4j.Logger;
 import rx.Observable;
 
-import static org.slf4j.LoggerFactory.getLogger;
-
+@FunctionalInterface
 public interface SearcherErrorHandler {
 
-    Logger LOGGER = getLogger(SearcherErrorHandler.class);
+    Observable<Offer> searcherError(SearcherException t);
 
-    default Observable<Offer> searcherError(SearcherException t) {
-        LOGGER.warn("Error in searcher", t);
-        return Observable.empty();
-    }
 }
